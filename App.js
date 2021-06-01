89fc22276af63f27ea4917e1182e8980308d1749
Soham2008 <sohamswarup2008@gmail.com>
2021-06-01 20:51:03 +0530

    first commit

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import Facebook from './screens/fb';
 import Instagram from './screens/in';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default class App extends React.Component {
   render() {
     return (
+      <SafeAreaProvider>
       <AppContainer/>
+      </SafeAreaProvider>
     );
   }
 }
--- screens/fb.js
+++ screens/fb.js
@@ -1,36 +1,71 @@
 import * as React from "react";
 import { View, Text } from "react-native";
 import { Header } from "react-native-elements";
+
 export default class Facebook extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <Header
-          backgroundColor={"#E60576"}
-          centerComponent={{
-            text: "Buzz App",
-            style: { fontSize: 28, color: "white" },
-          }}
-        />
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: "center",
-              color: "blue",
-              fontWeight: "bold",
-            }}
-          >
-            FACEBOOK
+
+    render() {
+
+        return (
+
+            <View style={
+
+                { flex: 1 }}>
+
+                <Header
+                    backgroundColor={"#ffbfbd"}
+
+                    centerComponent={
+
+                        {
+
+                            text: "Buzz App",
+                            style: { fontSize: 28, color: "black" },
+
+                        }
+
+                    }
+                />
+
+                <View
+                    style={
+
+                        {
+
+                            flex: 1,
+                            justifyContent: "center",
+                            alignItems: "center"
+
+                        }
+
+                    }
+                >
+
+                    <Text
+                        style={
+
+                            {
+
+                                fontSize: 20,
+                                textAlign: "center",
+                                color: "#745bc3",
+                                fontWeight: "bold",
+
+                            }
+
+                        }
+                    >
+
+                        Facebook
+
           </Text>
-        </View>
-      </View>
-    );
-  }
+
+                </View>
+
+            </View>
+
+        );
+
+    }
+
 }
